86cec50fed8cdf3fae7faa3e1fc36f9c9221f6e8
HussainALAradi5 <hussainaradi.ha@gmail.com>
2025-05-31 02:59:56 +0300

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, path.resolve(__dirname, '../.env'))
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env,
-      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
-    }
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
