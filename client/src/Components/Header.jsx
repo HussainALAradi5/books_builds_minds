5bd05cb31943b183382a1e3b3adeb6b3155389f4
HussainALAradi5 <hussainaradi.ha@gmail.com>
2025-06-20 01:48:45 +0300

    solve some styloing issue in the haeder

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,6 +1,9 @@
 import "../styles/header.css"
 const Header = () => {
-  return <div className="header">header</div>
+  return <div className="header-container">
+    <div className="header"> header</div>
+   
+    </div>
 }
 
 export default Header
