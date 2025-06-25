14886d99ab5f7437f880141e087b0f447e1f9f09
HussainALAradi5 <hussainaradi.ha@gmail.com>
2025-06-25 23:27:23 +0300

    add auth to prevent unAutherize user from entering the admin panel

--- client/src/Pages/AdminPanelPage.jsx
+++ client/src/Pages/AdminPanelPage.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchUserProfile } from "../../service/auth";
 import AdminPanel from "../Components/AdminPanel";
 
 const AdminPanelPage = () => {
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userId = localStorage.getItem("user_id");
+    const token = localStorage.getItem("token");
+
+    if (!userId || !token) {
+      navigate("/"); // Not logged in
+      return;
+    }
+
+    fetchUserProfile(userId, token)
+      .then((data) => {
+        if (!data.is_admin) {
+          navigate("/");
+        } else {
+          setUserData(data);
+        }
+      })
+      .catch((err) => {
+        console.error("Failed to fetch user profile:", err.message);
+        navigate("/");
+      });
+  }, [navigate]);
+
+  if (!userData) return <p>Checking admin access...</p>;
+
   return (
     <div>
       <AdminPanel />
