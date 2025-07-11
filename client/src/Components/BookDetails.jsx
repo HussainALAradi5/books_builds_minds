ecb7479b173d647b817b2fe584592fec63b47c7f
HussainALAradi5 <hussainaradi.ha@gmail.com>
2025-07-11 21:34:03 +0300

    some minor imporvement

--- client/src/Components/BookDetails.jsx
+++ client/src/Components/BookDetails.jsx
@@ -1,14 +1,17 @@
+import formatPublishedDate from "../utilities/formatDate";
 const BookDetails = ({ book }) => {
   if (!book) return null;
 
   const bookImage = book.image || book.book_image;
 
+  const publishedAt = formatPublishedDate(book.published_at);
   return (
     <>
       <h3>{book.title}</h3>
       {bookImage && <img className="cover" src={bookImage} alt={book.title} />}
       <p>Author: {book.author}</p>
       <p>Publisher: {book.publisher}</p>
+      <p>Published at : {publishedAt}</p>
       <p>Price: {book.price} BD</p>
     </>
   );
---
+++ client/src/utilities/formatDate.js
@@ -0,0 +1,10 @@
+const formatPublishedDate = (dateString) => {
+  const date = new Date(dateString);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+export default formatPublishedDate;
